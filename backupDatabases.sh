d1dab15cae7ce5d67ff21d37ac5cc0d7b8b5ba63
jb-alvarado <jb@pixelcrusher.de>
2015-09-09 10:01:39 +0200

    Update backupDatabases.sh

--- backupDatabases.sh
+++ backupDatabases.sh
@@ -8,7 +8,7 @@ BACKUP_DIR="/dir/to/backup/location"
 for db in $(mysql -B -s -u $MYSQL_USER --password=$MYSQL_PASS -e 'show databases' | grep -Ev 'information_schema|performance_schema|sitebuilder5|mysql|phpmyadmin' ); do
   # dump each database in a separate file
   echo "backup database: \"$db\""
-  mysqldump -u $MYSQL_USER --password=$MYSQL_PASS --events --skip-lock-tables "$db" | gzip > "$BACKUP_DIR/$db.sql_$(date +%Y-%m-%d_%H-%M).gz"
+  mysqldump -u $MYSQL_USER --password=$MYSQL_PASS --events --skip-lock-tables "$db" | gzip > "$BACKUP_DIR/${db}_$(date +%Y-%m-%d).sql.gz"
 done
 
 exit
