4e8cca475645984f34bdfabcdc8f549815db98ab
jb-alvarado <jb@pixelcrusher.de>
2015-09-09 09:13:53 +0200

    Create backupDatabases.sh
    check all databases from mysql and buckup them in different archives.

---
+++ backupDatabases.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+MYSQL_USER="<user>"
+MYSQL_PASS="<password>"
+BACKUP_DIR="/dir/to/backup/location"
+
+# Get the database list, exclude information_schema
+for db in $(mysql -B -s -u $MYSQL_USER --password=$MYSQL_PASS -e 'show databases' | grep -Ev 'information_schema|performance_schema|sitebuilder5|mysql|phpmyadmin' ); do
+  # dump each database in a separate file
+  echo "backup database: \"$db\""
+  mysqldump -u $MYSQL_USER --password=$MYSQL_PASS --events --skip-lock-tables "$db" | gzip > "$BACKUP_DIR/$db.sql_$(date +%Y-%m-%d_%H-%M).gz"
+done
+
+exit
